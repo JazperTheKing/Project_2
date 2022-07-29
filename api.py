f5c31abb4759d57fe853d56f3ada965607d8dc62
nataliechng <110211114+nataliechng@users.noreply.github.com>
2022-07-29 12:39:03 +0800

    Update api.py

--- api.py
+++ api.py
@@ -1,6 +1,14 @@
 import requests
-url = 'https://www.alphavantage.co/documentation'
-r = requests.get(url)
-print(r)
-data = r.read()
-print(data)
+import json
+
+url = 'https://www.alphavantage.co'
+response = requests.get(url)
+print(response)
+#print(response.headers.get('Content-Type'))
+#data = json.load(response)
+data = response.json()
+print(data.keys())
+#print(type(data))
+#def currency_exchange_rate():
+   # "from Symbol": "EUR",
+    #"3. To Symbol": "USD",
