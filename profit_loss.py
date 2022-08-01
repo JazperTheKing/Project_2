4de71008978aaa141a7e0533a2debc91af8f3acf
peiying06 <110211813+peiying06@users.noreply.github.com>
2022-08-02 03:41:08 +0800

    Update profit_loss.py

--- profit_loss.py
+++ profit_loss.py
@@ -1,17 +1,22 @@
-import requests
 from pathlib import Path
 import re, csv
+from collections import defaultdict
 
-file_path = Path.cwd()/"csv_reports"/"profit-and-loss-usd.csv"
+print(Path.cwd())
+
+file_path = Path.cwd()/"csv_reports"/"profit-and-loss-usd .csv"
 print(file_path.exists())
 print(file_path)
 
+columns = defaultdict(list)
 
 # Open the profit-and-loss-usd csv file to read it
 with file_path.open(mode="r", encoding = "UTF-8", newline="") as file:
   reader = csv.reader(file)
   next(file)
 
-  for line in reader:
-   print(line)
+  for row in reader:
+     for (i,v) in enumerate(row):
+            columns[i].append(v)
+print(columns[4])
    
