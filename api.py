7fc94961555d9e2de3d5e5903f163f9f347b7a05
nataliechng <110211114+nataliechng@users.noreply.github.com>
2022-08-03 16:31:43 +0800

    Update api.py

--- api.py
+++ api.py
@@ -2,21 +2,20 @@ import requests, re
 from pathlib import Path
 import json
 
-function = 'FX_WEEKLY'
+function = 'CURRENCY_EXCHANGE_RATE'
 from_currency = 'USD'
 to_currency = 'SGD'
 api_key = 'TYRNR04ZRXIKTHMK'
 base_url = 'https://www.alphavantage.co'
-main_url = base_url+'/query?function='+function+'&from_symbol='+from_currency+'&to_symbol='+to_currency+'&apikey='+api_key
+main_url = base_url+'/query?function='+function+'&from_currency='+from_currency+'&to_currency='+to_currency+'&apikey='+api_key
 response = requests.get(main_url).json()
-d = response["Time Series FX (Weekly)"]
-recentdate = max((x for x in d.keys()))
-print(recentdate)
-number1 = float(d[recentdate]["2. high"])
-number2 = float(d[recentdate]["3. low"])
-mean = (number1+number2)/2
-print(mean)
+print(response)
+for item in response:
+    rate = float(response[item]["5. Exchange Rate"])
+    fcurrency = response[item]["1. From_Currency Code"]
+    tcurrency = response[item]["3. To_Currency Code"]
+
 
 file_path = Path.cwd()/"project_group"/"main.py"
 with file_path.open(mode="w", encoding="UTF-8", newline="") as file:
-    file.write("[MEAN WEEKLY CLOSING FOREX PRICE]" " " f"{from_currency} = {to_currency}{mean}")
+    file.write("[REAL TIME CURRENCY CONVERSION RATE]" " " f"{fcurrency}1 = {tcurrency}{rate}")
