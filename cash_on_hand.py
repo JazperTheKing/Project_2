88eb43be72354df37ece57d267942897269848fe
Asyikin01 <S10247675@connect.np.edu.sg>
2022-08-03 21:53:36 +0800

    update

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,7 +1,11 @@
 # import requests, re, csv module, pathlib
+from ast import operator
+from tkinter.tix import INCREASING
 import requests
 from pathlib import Path
 import re, csv
+
+
 
 # check file path of current working directory
 print(Path.cwd())
@@ -15,21 +19,29 @@ print(filepath)
 # Open file using 'with' and 'open' keyword in 'read' mode
 with filepath.open('r', encoding = 'UTF-8', newline = '') as csv_file:
     csv_reader = csv.reader(csv_file)
-
     next(csv_reader)
 
-    for line in csv_reader:
-        print(line)
-    #     if day 
+    amount = []
+
+    #     if list[i] < list[i +1]:
+    #         csv_file = True
         
-    #         print("[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
-
-
     #     else:
-
-    #         print("[CASH DEFICIT] DAY: {day} , AMOUNT: {amount}")
-
-
-
+    #         csv_file = False
     
+    # if csv_file == True:
+    #     print("[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
 
+    # else: 
+    #     print("[CASH DEFICIT] DAY: , AMOUNT:")
+
+
+
+
+    # max_value = max(row[0] for row in csv.reader(csv_file))
+    # #print("[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
+    # print(max_value)
+    
+    # min_value = min(row[0] for row in csv.reader(csv_file))
+    # #print(f"[CASH DEFICIT] DAY: , AMOUNT:")
+    # print(min_value)
