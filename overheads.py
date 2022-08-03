de54e3fc30f7215c2f166deb28caf10dd719b1b1
Asyikin01 <S10247675@connect.np.edu.sg>
2022-08-04 00:01:55 +0800

    update

--- overheads.py
+++ overheads.py
@@ -12,21 +12,10 @@ print(filepath.exists())
 print(filepath)  
 
 # Open file using 'with' and 'open' keyword in 'read' mode
-# with filepath.open('r', encoding = 'UTF-8', newline = '') as csv_file:
-#     csv_reader = csv.reader(csv_file, delimiter=";")
-#     category = 0
-#     next(csv_reader)
-#     csv_reader = []
-#     result = max(column[0,1].replace(",", "") for column in csv_reader)
-#         #max_overheads = max(row[0] for row in csv.reader(csv_file))
-# print(f'[HIGHEST OVERHEADS] {", ".join(result)}')
-
-
 with filepath.open('r', encoding = 'UTF-8', newline = '') as csv_file:
-    #csv_reader = csv.reader(csv_file, delimiter=";")
-    data = csv.DictReader(csv_file)
-    #next(csv_reader)   
-    print(f'[HIGHEST OVERHEADS] {", ".join(data)}')
-
-
-
+    csv_reader = csv.reader(csv_file, delimiter=";")
+    category = 0
+    next(csv_reader)
+    for row in csv_reader:
+       max_overheads = max(row[0] for row in csv.reader(csv_file))
+       print(f'[HIGHEST OVERHEADS] {", ".join(row)}')
