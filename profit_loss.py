4e71235f3388fed23f477c641c910ab85c23c9db
peiying06 <110211813+peiying06@users.noreply.github.com>
2022-08-04 03:43:26 +0800

    Update profit_loss.py

--- profit_loss.py
+++ profit_loss.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 import re, csv
-from collections import defaultdict
 
 print(Path.cwd())
 
@@ -8,15 +7,13 @@ file_path = Path.cwd()/"csv_reports"/"profit-and-loss-usd .csv"
 print(file_path.exists())
 print(file_path)
 
-columns = defaultdict(list)
+def profit_loss(forex):
+  with file_path.open(mode="r", encoding = "UTF-8", newline="") as file:
+    reader = csv.reader(file)
+    next(file)
 
-# Open the profit-and-loss-usd csv file to read it
-with file_path.open(mode="r", encoding = "UTF-8", newline="") as file:
-  reader = csv.reader(file)
-  next(file)
-
-  for row in reader:
-     for (i,v) in enumerate(row):
-            columns[i].append(v)
-print(columns[4])
+    
+    for line in reader:
+      net_profit = print(line[0], line[4])
+    return net_profit
 (not complete)
