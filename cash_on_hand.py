59486166703c8d4d9e3fc539a407b955d166ebf2
Asyikin01 <S10247675@connect.np.edu.sg>
2022-08-03 19:42:08 +0800

    update

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -11,23 +11,23 @@ filepath = Path.cwd()/"csv_reports"/"cash-on-hand-usd.csv"
 print(filepath.exists())
 print(filepath)
 
-### not complete ###
-# Open file using 'with' and 'open' keyword in 'read' mode
-# with filepath.open('r', encoding = 'UTF-8', newline = '') as csv_file:
-#     csv_reader = csv.reader(csv_file)
 
-#     next(csv_reader)
+Open file using 'with' and 'open' keyword in 'read' mode
+with filepath.open('r', encoding = 'UTF-8', newline = '') as csv_file:
+    csv_reader = csv.reader(csv_file)
 
-#     for line in csv_reader:
-#         print(line)
-#         if day 
+    next(csv_reader)
+
+    for line in csv_reader:
+        print(line)
+        if day 
         
-#             print("[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
+            print("[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
 
 
-#         else:
+        else:
 
-#             print("[CASH DEFICIT] DAY: {day} , AMOUNT: {amount}")
+            print("[CASH DEFICIT] DAY: {day} , AMOUNT: {amount}")
 
 
 
