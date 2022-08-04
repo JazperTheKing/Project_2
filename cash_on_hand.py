723662427da6d2757b0cd1e13b0deb8e402cdf83
Asyikin01 <S10247675@connect.np.edu.sg>
2022-08-04 18:01:48 +0800

    update

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,7 +1,8 @@
 # import requests, re, csv module, pathlib
 import requests
 from pathlib import Path
-import re, csv
+import re
+import csv
 
 # check file path of current working directory
 print(Path.cwd())
@@ -11,37 +12,45 @@ filepath = Path.cwd()/"csv_reports"/"cash-on-hand-usd.csv"
 print(filepath.exists())
 print(filepath)  
 
-
 with filepath.open('r', encoding = 'UTF-8', newline = '') as csv_file:
     csv_reader = csv.reader(csv_file)
     next(csv_reader)
-    category = []
+    day = []
     cash_on_hand = []
     coh_change = []
 
+    for row in csv_reader:
+        day.append(row[0])
+
     for i in range (1, len(cash_on_hand)):
         coh_change.append(cash_on_hand[i] - cash_on_hand[i-1])
-        print(coh_change)
+        #coh_change = ("Cash On Hand")-("Cash On Hand").shift(1)
+        max_coh_change = max(coh_change)
+        max_coh_day = str(day[coh_change.index(max(coh_change))])
+    
+    print(f"[CASH DEFICIT] DAY: {max_coh_day} AMOUNT {max_coh_change}")
+
+    
+
+# "[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY"
+# return "[CASH DEFICIT] DAY: , AMOUNT:"
 
 
+# def coh_function():
+#     with filepath.open('r', encoding = 'UTF-8', newline = '') as csv_file:
+#         csv_reader = csv.reader(csv_file)
+#         next(csv_reader)
+#         category = []
+#         cash_on_hand = []
+#         coh_change = []
+
+#     for i in range (1, len(cash_on_hand)):
+#         #coh_change.append(cash_on_hand[i] - cash_on_hand[i-1])
+#         coh_change = ["Cash On Hand"]-["Cash On Hand"].shift(1)
 
 
+#     file_path = Path.cwd()/"summary_report.txt"
+#     with file_path.open(mode="w", encoding="UTF-8", newline="") as file:
+#          file.write(coh_change)
 
-
-# Open file using 'with' and 'open' keyword in 'read' mode
-# with filepath.open('r', encoding = 'UTF-8', newline = '') as csv_file:
-#     csv_reader = csv.reader(csv_file)
-#     next(csv_reader)
-#     def solve(csv_reader):
-#         if csv_reader[1,1] <= csv_reader[0,1]:
-#             return "[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY"
-
-#         for i in range(len(csv_reader)):
-#             if i - 1 >= 0:
-#                 if csv_reader[i] == csv_reader[i-1]:
-#                     return "[CASH DEFICIT] DAY: , AMOUNT:"
-
-
-# print(solve(csv_reader))
-
-
+# print(coh_function())
