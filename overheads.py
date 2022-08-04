50b7be7541cd231855c4e5290299252ed72d50ad
Asyikin01 <S10247675@connect.np.edu.sg>
2022-08-04 19:00:41 +0800

    upd

--- overheads.py
+++ overheads.py
@@ -1,7 +1,7 @@
 # import modules
 import requests
 from pathlib import Path
-import csv, re, api
+import csv, re
 
 # check file path of current working directory
 print(Path.cwd())
@@ -21,19 +21,20 @@ max_empty_list = []
 api_list = []
 
 # Start of a function
-def overheads():
+def overheads(forex):
     # Open file using 'with' and 'open' keyword in 'read' mode
     with fp_read.open(mode= "r", encoding= "UTF-8") as file:
-        coh_reader = csv.reader(file)
-        next(coh_reader)
+        oh_reader = csv.reader(file)
+        next(oh_reader)
 
-    for line in coh_reader:
+    for line in oh_reader:
         overheads_empty_list.append(line)
         cat = line[0]
         amt = line[1]
         cat_empty_list.append(int(cat))
         amt_empty_list.append(amt)
 
+    # Open file using 'with' and 'open' keyword in 'read' mode
     with fp_get.open(mode= "r", encoding= "UTF-8") as file:
         api_get = file.read()
         api_list.append(api_get)
@@ -47,11 +48,12 @@ def overheads():
             for sublist in max_empty_list:
                 usd_to_sgd = sublist * forex
 
+    # Open file using 'with' and 'open' keyword in 'append' mode
     with fp_write.open(mode= "a", encoding= "UTF-8", newline= "") as file:    
         for category in zip(cat_empty_list, amt_empty_list):
-            file.write("\n[HIGHEST OVERHEADS]" " "f"DAY: {category[0]+1}, AMOUNT: SGD{usd_to_sgd}")
+            file.write("\n[HIGHEST OVERHEADS] " " "f"DAY: {category[0]+1}, AMOUNT: SGD{usd_to_sgd}")
             
-print(overheads())
+print(overheads(forex))
 
 
 # # start of a function
