33a7a28651edc2c74eadb8be9244f30052a46493
peiying06 <110211813+peiying06@users.noreply.github.com>
2022-08-04 23:54:00 +0800

    Update profit_loss.py

--- profit_loss.py
+++ profit_loss.py
@@ -48,6 +48,7 @@ def profit_and_loss():
     # open "summary_report.txt" file using 'with' and 'open' keyword in 'append' mode 
     with fp_write.open(mode= "a", encoding= "UTF-8", newline= "") as file:    
       for category in zip(days_empty_list, diff_empty_list):
+        # using if funcion, if the catergory is less than 0,
         if category[1] <= 0:
           file.write("\n[NET PROFIT DEFICIT]" " "f"DAY: {category[0]}, AMOUNT: SGD{category[1]*forex}")
           # else if the category is more than 0, it will print this
