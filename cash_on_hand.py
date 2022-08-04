c71359332cf2126447182aff9448759fec168b66
ddaniielle <110211803+ddaniielle@users.noreply.github.com>
2022-08-04 23:30:24 +0800

    Create cash_on_hand.py

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,28 +1,38 @@
+# importing built-in functions from python
 import csv, re
 from pathlib import Path
 
+# creating function for cash on hand
 def cash_on_hand():
+    # reading of csv file extracted from game
     fp_read = Path.cwd()/"csv_reports"/"cash-on-hand-usd.csv"
+    # creating variable to write to summary text file
     fp_write = Path.cwd()/"summary_report.txt"
+    # creating empty lists
     coh_empty_list = []
     days_empty_list = []
     amt_empty_list = []
     diff_empty_list = []
     exchange_rate = []
 
-
+    # opening csv file to read
     with fp_read.open(mode= "r", encoding= "UTF-8") as file:
         coh_reader = csv.reader(file)
         next(coh_reader)
-
+        
+        # creating for loop 
         for line in coh_reader:
+            # appending to empty list
             coh_empty_list.append(line)
+            # line[0] is the days column
             day = line[0]
+            # line[1] is the cash on hand amount column
             amt = line[1]
+            # appending to empty lists
             days_empty_list.append(int(day))
             amt_empty_list.append(amt)
 
-
+    # opening summary text file as read to read the data inside
     with fp_write.open(mode= "r", encoding= "UTF-8") as file:
         summary_get = file.read()
         exchange_rate.append(summary_get)
@@ -30,9 +40,6 @@ def cash_on_hand():
         for info, content in enumerate(exchange_rate):
             forex = re.search(pattern= "SGD.+\d", string=content)
             forex = forex.group()
-            if forex is None:
-                return None
-            
             forex = float(forex[3:10])
             
             for items in range(len(amt_empty_list)):
