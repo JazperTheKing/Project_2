446872f305f4878877348350b8cb920dbe84a47e
JazperTheKing <S10243739@connect.np.edu.sg>
2022-08-04 17:48:03 +0800

    ommt

--- profit_loss.py
+++ profit_loss.py
@@ -12,21 +12,8 @@ def profit_loss(forex):
     reader = csv.reader(file)
     next(file)
 
-<<<<<<< HEAD
-# Open the profit-and-loss-usd csv file to read it
-with file_path.open(mode="r", encoding = "UTF-8", newline="") as file:
-  reader = csv.reader(file)
-  next(file)
-
-  for row in reader:
-     for (i,v) in enumerate(row):
-            columns[i].append(v)
-print(columns[4])
-
-=======
     
     for line in reader:
       net_profit = print(line[0], line[4])
     return net_profit
 (not complete)
->>>>>>> a5ab80a48fd5561160d077a1dccf5e5e4bd51a24
