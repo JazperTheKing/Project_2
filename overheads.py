57819f19eb463c3db371e07e7a10e17ba0a06562
Asyikin01 <S10247675@connect.np.edu.sg>
2022-08-04 18:52:22 +0800

    update

--- overheads.py
+++ overheads.py
@@ -1,23 +1,68 @@
+# import modules
 import requests
 from pathlib import Path
-import csv
+import csv, re
 
 # check file path of current working directory
 print(Path.cwd())
 
-filepath = Path.cwd()/"csv_reports"/"overheads-day-42.csv"
-# check if the new file path exists will return True since "cash-on-hand-usd.csv" exists in the filepath
-print(filepath.exists())
-print(filepath)  
+fp_get = Path.cwd()/"api.py"
+fp_write = Path.cwd()/"summary_report.txt"
+fp_read = Path.cwd()/"csv_reports"/"overheads-day-42.csv"
+# Checking if a file/directory exists with Pathlib
+# Returns: True if a file/directory exists, False if a file/directory does not exit
+print(fp_read.exists())
 
-def overhead_function():
+# Create a list
+overheads_empty_list = []
+cat_empty_list = []
+amt_empty_list = []
+max_empty_list = []
+api_list = []
 
+# Start of a function
+def overheads(forex):
     # Open file using 'with' and 'open' keyword in 'read' mode
-    with filepath.open('r', encoding = 'UTF-8', newline = '') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=";")
-        category = 0
-        next(csv_reader)
-    for row in csv_reader:
-            max_overheads = max(row[0] for row in csv.reader(csv_file))
-            print(f'[HIGHEST OVERHEADS] {", ".join(row)}')
-print(overhead_function())
+    with fp_read.open(mode= "r", encoding= "UTF-8") as file:
+        coh_reader = csv.reader(file)
+        next(coh_reader)
+
+    for line in coh_reader:
+        overheads_empty_list.append(line)
+        cat = line[0]
+        amt = line[1]
+        cat_empty_list.append(int(cat))
+        amt_empty_list.append(amt)
+
+    with fp_get.open(mode= "r", encoding= "UTF-8") as file:
+        api_get = file.read()
+        api_list.append(api_get)
+
+        for info in enumerate(api_list):
+            forex = re.search(pattern= "function", float=info)
+        
+        for items in range(1, len(amt_empty_list)):
+            max = float(max(amt_empty_list))
+            max_empty_list.append(max)
+            for sublist in max_empty_list:
+                usd_to_sgd = sublist * forex
+
+    with fp_write.open(mode= "a", encoding= "UTF-8", newline= "") as file:    
+        for category in zip(cat_empty_list, amt_empty_list):
+            file.write("\n[HIGHEST OVERHEADS]" " "f"DAY: {category[0]+1}, AMOUNT: SGD{usd_to_sgd}")
+            
+print(overheads(forex))
+
+
+# # start of a function
+# def overhead_function():
+#     # Open file using 'with' and 'open' keyword in 'read' mode
+#     with fp_read.open('r', encoding = 'UTF-8', newline = '') as csv_file:
+#         csv_reader = csv.reader(csv_file, delimiter=";")
+#         category = 0
+#         #
+#         next(csv_reader)
+#     for row in csv_reader:
+#             max_overheads = max(row[0] for row in csv.reader(csv_file))
+#             print(f'[HIGHEST OVERHEADS] {", ".join(row)}')
+# print(overhead_function())
