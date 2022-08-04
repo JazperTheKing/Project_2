8822cc856fd7eb84c09619c657633e6321bcf638
nataliechng <110211114+nataliechng@users.noreply.github.com>
2022-08-04 23:45:14 +0800

    Update cash_on_hand.py

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -34,24 +34,39 @@ def cash_on_hand():
 
     # opening summary text file as read to read the data inside
     with fp_write.open(mode= "r", encoding= "UTF-8") as file:
+        #read the opened file
         summary_get = file.read()
+        #combind the open fil into an empty list
         exchange_rate.append(summary_get)
 
+        #using for loop with enumerateto in the empty list
         for info, content in enumerate(exchange_rate):
+            #we use re.search to find the pattern of SGD and extract it
             forex = re.search(pattern= "SGD.+\d", string=content)
+            #then we group the extracted data
             forex = forex.group()
+            #and get rid of the SGD by cutting the search, so that python recognizes forex as a float
             forex = float(forex[3:10])
             
+            #using for loop to find the range and length of the amount empty list
             for items in range(len(amt_empty_list)):
+                #create a string to minus the 2 numbers
                 diff = float(amt_empty_list[items]) - float(amt_empty_list[items-1])
+                #put the difference between the numbers into an empty list and append the list
                 diff_empty_list.append(diff)
+                #times the appened list with forex
                 usd_to_sgd = diff_empty_list[-1] * forex
                     
+         #open the main file as append            
         with fp_write.open(mode= "a", encoding= "UTF-8", newline= "") as file:    
+            #create for loop to create a zip for 2 appened list creates previousely as category
             for category in zip(days_empty_list, diff_empty_list):
+                #using if funcion, if the catergory is less than 0, 
                 if category[1] <= 0:
+                    #the file will print this
                     file.write("\n[CASH DEFICIT]" " "f"DAY: {category[0]}, AMOUNT: SGD{category[1]*forex}")
                 else:
+                    #else if the category is more than 0, it will print this
                     file.write("\n[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
 
 print(cash_on_hand())
