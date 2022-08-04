bcd3bace0bcd8863b0f4db97b0265b1019dfbcf4
Asyikin01 <S10247675@connect.np.edu.sg>
2022-08-04 19:13:18 +0800

    amend

--- profit_loss.py
+++ profit_loss.py
@@ -1,14 +1,30 @@
+# import modules
 from pathlib import Path
 import re, csv
 
+# check file path of current working directory
+print(Path.cwd())
 
-file_path_read = Path.cwd()/"csv_reports"/"profit-and-loss-usd.csv"
-file_path_write = Path.cwd()/"summary_report.txt"
-file_path_get = Path.cwd()/"api.py"
+#from api_test import function
+fp_get = Path.cwd()/"api.py"
+fp_write = Path.cwd()/"summary_report.txt"
+fp_read = Path.cwd()/"csv_reports"/"profit-and-loss-usd.csv"
+# Checking if a file/directory exists with Pathlib
+# Returns: True if a file/directory exists, False if a file/directory does not exit
+print(fp_read.exists())
 
+# Create a list
+
+
+
+
+
+# Start of a function
 def profit_loss(forex):
-  with file_path_read.open(mode="r", encoding = "UTF-8", newline="") as file:
+  # Open file using 'with' and 'open' keyword in 'read' mode
+  with fp_read.open(mode="r", encoding = "UTF-8", newline="") as file:
     reader = csv.reader(file)
+    # Use of next to skip first header row in csv file
     next(file)
 
     net_profit= []
@@ -19,7 +35,8 @@ def profit_loss(forex):
       net_profit.append(int(line[4]))
 
       api_list = []
-      with file_path_get.open(mode= "r", encoding= "UTF-8") as file:
+       # Open file using 'with' and 'open' keyword in 'read' mode
+      with fp_get.open(mode= "r", encoding= "UTF-8") as file:
         api_get = file.read()
         api_list.append(api_get)
 
@@ -31,11 +48,12 @@ def profit_loss(forex):
           for sublist in net_profit_diff:
                 usd_to_sgd = sublist * forex
 
-      with file_path_write.open(mode= "a", encoding= "UTF-8", newline= "") as file:
+      # Open file using 'with' and 'open' keyword in 'append' mode
+      with fp_write.open(mode= "a", encoding= "UTF-8", newline= "") as file:
         for category in zip(day, net_profit_diff):
             if category[1] <= 0:
               file.write("\n[PROFIT DEFICIT]" " "f"DAY: {category[0]+1}, AMOUNT: SGD{usd_to_sgd}")
             else:
                 file.write("\n[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
 
-print(profit_loss(forex))
+  print(profit_loss(forex))
