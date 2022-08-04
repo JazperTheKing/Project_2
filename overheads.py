9fa5883836b3fd2f26f219d9635f8e07c6f9db40
Asyikin01 <S10247675@connect.np.edu.sg>
2022-08-04 19:51:26 +0800

    update

--- overheads.py
+++ overheads.py
@@ -1,28 +1,28 @@
 # import modules
-import requests
-from pathlib import Path
 import csv, re
+from pathlib import Path
 
 # check file path of current working directory
 print(Path.cwd())
 
 #from api_test import function
-fp_write = Path.cwd()/"summary_report.txt"
-fp_get = Path.cwd()/"api.py"
-fp_read = Path.cwd()/"csv_reports"/"overheads-day-42.csv"
-# Checking if a file/directory exists with Pathlib
-# Returns: True if a file/directory exists, False if a file/directory does not exit
-print(fp_read.exists())
-
-# Create a list
-overheads_empty_list = []
-cat_empty_list = []
-amt_empty_list = []
-max_empty_list = []
-api_list = []
 
 # Start of a function
-def overheads(forex):
+def overheads():
+    fp_write = Path.cwd()/"summary_report.txt"
+    fp_get = Path.cwd()/"api.py"
+    fp_read = Path.cwd()/"csv_reports"/"overheads-day-42.csv"
+    # Checking if a file/directory exists with Pathlib
+    # Returns: True if a file/directory exists, False if a file/directory does not exit
+    print(fp_read.exists())
+
+    #Create a list
+    overheads_empty_list = []
+    cat_empty_list = []
+    amt_empty_list = []
+    max_empty_list = []
+    api_list = []
+
     # Open file using 'with' and 'open' keyword in 'read' mode
     with fp_read.open(mode= "r", encoding= "UTF-8") as file:
         oh_reader = csv.reader(file)
@@ -33,7 +33,7 @@ def overheads(forex):
         overheads_empty_list.append(line)
         cat = line[0]
         amt = line[1]
-        cat_empty_list.append(int(cat))
+        cat_empty_list.append(cat)
         amt_empty_list.append(amt)
 
     # Open file using 'with' and 'open' keyword in 'read' mode
@@ -41,19 +41,18 @@ def overheads(forex):
         api_get = file.read()
         api_list.append(api_get)
 
-        for info in enumerate(api_list):
-            forex = re.search(pattern= "function", float=info)
+        for info, content in enumerate(api_list):
+            forex = re.search(pattern= "SGD.+\d", string=content)
         
-        for items in range(1, len(amt_empty_list)):
+        for items in range(len(amt_empty_list)):
             max = float(max(amt_empty_list))
             max_empty_list.append(max)
-            for sublist in max_empty_list:
-                usd_to_sgd = sublist * forex
-
+            usd_to_sgd = max_empty_list[-1] * forex
+            
     # Open file using 'with' and 'open' keyword in 'append' mode
     with fp_write.open(mode= "a", encoding= "UTF-8", newline= "") as file:    
         for category in zip(cat_empty_list, amt_empty_list):
-            file.write("\n[HIGHEST OVERHEADS] " " "f"DAY: {category[0]+1}, AMOUNT: SGD{usd_to_sgd}")
-            
-    print(overheads(forex))
+            file.write("\n[HIGHEST OVERHEADS] " " "f"DAY: {category[0]}, AMOUNT: SGD{category[1]*forex}")
+               
+print(overheads())
 
