fc2305f13408d10b89dc330b86194842afdb9b11
Asyikin01 <S10247675@connect.np.edu.sg>
2022-08-04 17:23:55 +0800

    update

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -12,20 +12,36 @@ print(filepath.exists())
 print(filepath)  
 
 
-# Open file using 'with' and 'open' keyword in 'read' mode
 with filepath.open('r', encoding = 'UTF-8', newline = '') as csv_file:
     csv_reader = csv.reader(csv_file)
     next(csv_reader)
-    def solve(csv_reader):
-        if csv_reader[1,1] <= csv_reader[0,1]:
-            return "[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY"
+    category = []
+    cash_on_hand = []
+    coh_change = []
 
-        for i in range(len(csv_reader)):
-            if i - 1 >= 0:
-                if csv_reader[i] == csv_reader[i-1]:
-                    return "[CASH DEFICIT] DAY: , AMOUNT:"
+    for i in range (1, len(cash_on_hand)):
+        coh_change.append(cash_on_hand[i] - cash_on_hand[i-1])
+        print(coh_change)
 
 
-print(solve(csv_reader))
+
+
+
+
+# Open file using 'with' and 'open' keyword in 'read' mode
+# with filepath.open('r', encoding = 'UTF-8', newline = '') as csv_file:
+#     csv_reader = csv.reader(csv_file)
+#     next(csv_reader)
+#     def solve(csv_reader):
+#         if csv_reader[1,1] <= csv_reader[0,1]:
+#             return "[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY"
+
+#         for i in range(len(csv_reader)):
+#             if i - 1 >= 0:
+#                 if csv_reader[i] == csv_reader[i-1]:
+#                     return "[CASH DEFICIT] DAY: , AMOUNT:"
+
+
+# print(solve(csv_reader))
 
 
