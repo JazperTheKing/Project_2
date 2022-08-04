0c7a1e7059b9db2039f3f2386822af997bff2573
peiying06 <110211813+peiying06@users.noreply.github.com>
2022-08-04 23:44:28 +0800

    Update profit_loss.py

--- profit_loss.py
+++ profit_loss.py
@@ -1,20 +1,25 @@
 import csv, re
 from pathlib import Path
 
+# write a function: profit_and_loss() with no parameter 
 def profit_and_loss():
   fp_read = Path.cwd()/"csv_reports"/"profit-and-loss-usd.csv"
   fp_write = Path.cwd()/"summary_report.txt"
+  
+  # create 5 empty lists
   pnl_empty_list = []
   days_empty_list = []
   net_profit_empty_list = []
   diff_empty_list = []
   exchange_rate = []
 
-
+  # open 'profit-and-loss-usd.csv' file using 'with' and 'open' keyword in 'read' mode
   with fp_read.open(mode= "r", encoding= "UTF-8") as file:
     pnl_reader = csv.reader(file)
     next(pnl_reader)
 
+    # append the 'day' and 'net profit' columns of values into 'pnl_empty_list', before appending the two columns of data 
+    # into 2 different empty lists, 'days_empty_list' and 'net_profit_empty_list'
     for line in pnl_reader:
       pnl_empty_list.append(line)
       day = line[0]
@@ -22,11 +27,12 @@ def profit_and_loss():
       days_empty_list.append(int(day))
       net_profit_empty_list.append(net_profit)
 
-
+  # open "summary_report.txt" file using 'with' and 'open' keyword in 'read' mode
   with fp_write.open(mode= "r", encoding= "UTF-8") as file:
     summary_get = file.read()
     exchange_rate.append(summary_get)
-
+   
+    # 
     for info, content in enumerate(exchange_rate):
       forex = re.search(pattern= "SGD.+\d", string=content)
       forex = forex.group()
