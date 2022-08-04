5b3d46735cbfd1cf6318cc73eb2571ecd957ca10
ddaniielle <110211803+ddaniielle@users.noreply.github.com>
2022-08-04 23:40:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import api, cash_on_hand, overheads, profit_loss
 # Start of function
 def main():
 
-    forex = api.api_function()
-    overheads.overhead_function(forex)
-    cash_on_hand.cash_on_hand_function(forex)
-    profit_loss.profit_loss_function(forex)
+    api.api_function()
+    overheads.overheads()
+    cash_on_hand.cash_on_hand()
+    profit_loss.profit_and_loss()
     
-print(main())
+main()
